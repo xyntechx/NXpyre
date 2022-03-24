685c38b257ec3c745c6dc6ac405a25bafd8a3e0f
xyntechx <xyntechx@gmail.com>
2022-03-24 16:59:14 +0800

    🎉 Initialise pages

---
+++ pages/about.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Topnav from "../components/topnav/topnav";
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>NXpyre</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Inspire the Next Generation"
+                />
+                <meta name="author" content="Nyx Iskandar"></meta>
+                <meta property="og:title" content="NXpyre" />
+                <meta
+                    property="og:description"
+                    content="Inspire the Next Generation"
+                />
+                <meta
+                    property="og:image"
+                    content="https://nxpyre.com/og-image.png"
+                />
+                <meta property="og:url" content="https://nxpyre.com/" />
+                <meta property="og:type" content="website" />
+            </Head>
+
+            <Topnav />
+
+            {/* <main className={styles.main}>
+                    <Title />
+                    <Curves />
+                    <Footer />
+                </main> */}
+
+                {/* id students, professionals, artists */}
+        </div>
+    );
+}
