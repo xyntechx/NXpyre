467a206f517407b6fc348c38e3134767892dfae7
xyntechx <xyntechx@gmail.com>
2022-03-24 22:05:26 +0800

    🎉 Auth and Account pages

---
+++ pages/account.js
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Topnav from "../components/topnav/topnav";
+import Account_s from "../components/account/account-s";
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export default function Account() {
+    const [session, setSession] = useState(null);
+
+    useEffect(() => {
+        setSession(supabase.auth.session());
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+    }, []);
+
+    return (
+        <div>
+            <Head>
+                <title>NXpyre</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Inspire the Next Generation"
+                />
+                <meta name="author" content="Nyx Iskandar"></meta>
+                <meta property="og:title" content="NXpyre" />
+                <meta
+                    property="og:description"
+                    content="Inspire the Next Generation"
+                />
+                <meta
+                    property="og:image"
+                    content="https://nxpyre.com/og-image.png"
+                />
+                <meta property="og:url" content="https://nxpyre.com/" />
+                <meta property="og:type" content="website" />
+            </Head>
+
+            <Topnav />
+
+            <main className={styles.main}>
+                {!session ? (
+                    <>
+                        <h1 className={styles.title}>
+                            Oops! Please log in/sign up first!
+                        </h1>
+                        <br />
+                        <Link href="/auth">
+                            <a className={styles.button}>Join NXpyre</a>
+                        </Link>
+                    </>
+                ) : (
+                    <Account_s key={session.user.id} session={session} />
+                )}
+            </main>
+        </div>
+    );
+}
---
+++ pages/auth.js
@@ -0,0 +1,81 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Topnav from "../components/topnav/topnav";
+import { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export default function Auth() {
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+
+    async function handleLogin(email) {
+        try {
+            setLoading(true);
+            const { error } = await supabase.auth.signIn(
+                { email },
+                { redirectTo: "https://nxpyre.com/account/" }
+            );
+            if (error) throw error;
+            alert("Check your email for your Magic Link!");
+        } catch (error) {
+            alert(error.error_description || error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+    return (
+        <div>
+            <Head>
+                <title>NXpyre</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Inspire the Next Generation"
+                />
+                <meta name="author" content="Nyx Iskandar"></meta>
+                <meta property="og:title" content="NXpyre" />
+                <meta
+                    property="og:description"
+                    content="Inspire the Next Generation"
+                />
+                <meta
+                    property="og:image"
+                    content="https://nxpyre.com/og-image.png"
+                />
+                <meta property="og:url" content="https://nxpyre.com/" />
+                <meta property="og:type" content="website" />
+            </Head>
+
+            <Topnav />
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>Sign Up</h1>
+
+                <br />
+                <br />
+
+                <input
+                    type="email"
+                    className={styles.input}
+                    name="id"
+                    placeholder="Your Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <br />
+
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleLogin(email);
+                    }}
+                    disabled={loading}
+                    className={styles.button}
+                >
+                    <span>{loading ? "Loading..." : "Let's Go!"}</span>
+                </button>
+            </main>
+        </div>
+    );
+}
